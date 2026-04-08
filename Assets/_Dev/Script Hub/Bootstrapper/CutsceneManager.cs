d7f8bb6a1840bf46b255ff136295b3adbc93fe21
caiiru <caiogome125@gmail.com>
2026-04-08 17:19:44 -0300

    Ajustes

--- Assets/_Dev/Script Hub/Bootstrapper/CutsceneManager.cs
+++ Assets/_Dev/Script Hub/Bootstrapper/CutsceneManager.cs
@@ -1,9 +1,17 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace proscryption
 {
     public class CutsceneManager : MonoBehaviour
     {
+        void Update()
+        {
+            if (Keyboard.current.anyKey.isPressed)
+            {
+                AppManager.Instance.ChangeAppState(AppState.Playing);
+            }
+        }
         public void EndAnimation()
         {
             AppManager.Instance.ChangeAppState(AppState.Playing);
