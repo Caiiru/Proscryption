edf971ac2ab10520267e4092b4ff9d975c4a0546
caiiru <caiogome125@gmail.com>
2026-04-24 19:20:28 -0300

    Refactor: HUD + player light Color

--- Assets/Scripts/Player/HUD/PlayerBulletCounterHUD.cs
+++ Assets/Scripts/Player/HUD/PlayerBulletCounterHUD.cs
@@ -10,6 +10,7 @@ namespace proscryption
     public class PlayerBulletCounterHUD : MonoBehaviour
     {
         public Transform bulletBackground;
+        public Transform bulletContainerTransform;
         public Transform StandardStanceTransform;
         public Transform BloodStanceTransform;
         public Transform LightStanceTransform;
@@ -17,9 +18,7 @@ namespace proscryption
         public GameObject bulletForeground;
         private BaseWeapon _weapon;
 
-        public bool _updateIcons;
-        float radius = 80f; // Adjust as needed
-        float angleOffset = 30f;
+        public float angleOffset = 30f;
 
         PlayerStance _currentStance = PlayerStance.Standard;
         GameObject[] bulletIcons;
@@ -39,15 +38,7 @@ namespace proscryption
             bulletIcons = new GameObject[BaseWeapon.MAX_BULLETS];
             for (int i = 0; i < BaseWeapon.MAX_BULLETS; i++)
             {
-                GameObject icon = Instantiate(bulletForeground, bulletBackground);
-                bulletIcons[i] = icon;
-
-                // move icons in circle based on maxbullets based on center of bulletbackground     
-                float angle = i * (360f / BaseWeapon.MAX_BULLETS) + (angleOffset * 3);
-                Vector3 offset = new Vector3(-Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad), 0) * radius;
-                icon.GetComponent<RectTransform>().anchoredPosition = offset;
-
-                // icon.GetComponentInChildren<TextMeshProUGUI>().text = i.ToString();
+                bulletIcons[i] = bulletContainerTransform.GetChild(i).gameObject;
             }
 
             StandardStanceTransform.gameObject.SetActive(true);
@@ -88,11 +79,11 @@ namespace proscryption
         {
             if (b_index >= 0 && b_index < bulletIcons.Length)
             {
-                bulletIcons[b_index].GetComponent<UnityEngine.UI.Image>().color = EmptyColor;
+                // bulletIcons[b_index].GetComponent<UnityEngine.UI.Image>().color = EmptyColor;
+                bulletIcons[b_index].transform.gameObject.SetActive(false);
             }
             UniTask.Delay(2000).Forget();
-            bulletBackground.DORotate(new Vector3(0, 0, angleOffset * (b_index + 1) * 2), 0.5f, RotateMode.Fast); // Rotate the entire counter
-            _updateIcons = false;
+            bulletBackground.DORotate(new Vector3(0, 0, angleOffset * (b_index + 1) * 2), 0.5f, RotateMode.Fast); // Rotate the entire counter 
             return UniTask.CompletedTask;
         }
         private void Reload()
@@ -105,17 +96,7 @@ namespace proscryption
 
         }
 
-        void Update()
-        {
-            if (_updateIcons) return;
 
-            if (Keyboard.current.tKey.wasPressedThisFrame)
-            {
-                _updateIcons = true;
-                HandleAttackPlayed();
-
-            }
-        }
         private void HandleStanceChanged(PlayerStance oldStance, PlayerStance newStance)
         {
             // Debug.Log($"Stance changed from {oldStance} to {newStance}");
@@ -131,17 +112,17 @@ namespace proscryption
                     // bulletBackground.GetComponent<UnityEngine.UI.Image>().color = ColorUtility.TryParseHtmlString("#3C3C3C", out Color standardColor) ? standardColor : Color.gray;
                     bulletBackground.gameObject.SetActive(true);
                     StandardStanceTransform.gameObject.SetActive(true);
-                    bulletsColor = StandardColor;
+                    // bulletsColor = StandardColor;
                     break;
                 case PlayerStance.Blood:
                     // bulletBackground.GetComponent<UnityEngine.UI.Image>().color = Color.red;
                     BloodStanceTransform.gameObject.SetActive(true);
-                    bulletsColor = BloodColor;
+                    // bulletsColor = BloodColor;
                     break;
                 case PlayerStance.Light:
                     // bulletBackground.GetComponent<UnityEngine.UI.Image>().color = Color.yellow;
                     LightStanceTransform.gameObject.SetActive(true);
-                    bulletsColor = LightColor;
+                    // bulletsColor = LightColor;
                     break;
             }
 
@@ -149,7 +130,7 @@ namespace proscryption
             {
                 if (_weapon.bullets[i] == 0) continue;
 
-                bulletIcons[i].GetComponent<UnityEngine.UI.Image>().color = bulletsColor;
+                // bulletIcons[i].GetComponent<UnityEngine.UI.Image>().color = bulletsColor;
             }
             _currentStanceColor = bulletsColor;
         }
