d946e8db56a34b42e403b9a45bd3d7fad079f946
Miguel Luís Teixeira Machado <miguelltmac@gmail.com>
2026-06-08 22:03:24 -0300

    FixDaAula_Caiiru

--- Assets/Scripts/Bootstrapper/CutsceneManager.cs
+++ Assets/Scripts/Bootstrapper/CutsceneManager.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Video;
 
 namespace proscryption
 {
     public class CutsceneManager : MonoBehaviour
     {
+        public VideoPlayer cutsceneVideo; 
+
+        void Awake()
+        {
+            cutsceneVideo.loopPointReached += OnVideoEnd;
+        }
+        void OnVideoEnd(VideoPlayer vp)
+        {
+            Debug.Log("Video Was Ended");
+            AppManager.Instance.ChangeAppState(AppState.Playing);
+
+        }
         void Update()
         {
             if (Keyboard.current.anyKey.isPressed)
             {
+                Debug.Log("Try to skip");
                 AppManager.Instance.ChangeAppState(AppState.Playing);
-            }
+            } 
         }
         public void EndAnimation()
         {
